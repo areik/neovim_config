4c06d5e9a56231e68b12781b2d45b5c4e850607e
areik <reik.shima@gmail.com>
2023-12-14 09:59:27 +0900

    first commit

---
+++ lua/config/lazy.lua
@@ -0,0 +1,492 @@
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+
+vim.opt.clipboard = "unnamedplus"
+
+-- windows wsl only settings
+-- but if you write if vim.fn.has("win64") == 1 then , system cant use clipboard.
+if vim.fn.has('wsl') == 1 then
+  vim.g.clipboard = {
+    name = "win32yank-wsl",
+    copy = {
+      ["+"] = "win32yank.exe -i --crlf",
+      ["*"] = "win32yank.exe -i --crlf"
+    },
+    paste = {
+      ["+"] = "win32yank.exe -o --crlf",
+      ["*"] = "win32yank.exe -o --crlf"
+    },
+    cache_enable = 0,
+  }
+end
+
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup({
+  'jose-elias-alvarez/null-ls.nvim',
+  "williamboman/mason.nvim",
+  'williamboman/mason-lspconfig',
+  'jayp0521/mason-null-ls.nvim',
+  "neovim/nvim-lspconfig",
+  'stevearc/dressing.nvim',
+  'tami5/lspsaga.nvim',
+  'ray-x/lsp_signature.nvim',
+  'onsails/lspkind-nvim',
+  'j-hui/fidget.nvim',
+  'hrsh7th/nvim-cmp',
+  'hrsh7th/cmp-nvim-lsp',
+  'hrsh7th/cmp-buffer',
+  'hrsh7th/cmp-path',
+  {
+    "L3MON4D3/LuaSnip",
+    -- follow latest release.
+    version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+    -- install jsregexp (optional!).
+    build = "make install_jsregexp",
+    dependencies = { "rafamadriz/friendly-snippets" },
+  },
+  "rafamadriz/friendly-snippets",
+  'onsails/lspkind.nvim',
+  'nvim-lua/plenary.nvim',
+  'nvim-telescope/telescope.nvim',
+  'nvim-treesitter/nvim-treesitter',
+  'sainnhe/gruvbox-material',
+  'lambdalisue/fern.vim',
+  'nvim-lualine/lualine.nvim',
+  'windwp/nvim-autopairs',
+  "elentok/format-on-save.nvim",
+  {
+    'romgrk/barbar.nvim',
+    dependencies = {
+      'lewis6991/gitsigns.nvim',     -- OPTIONAL: for git status
+      'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+    },
+    init = function() vim.g.barbar_auto_setup = false end,
+    opts = {
+      -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
+      -- animation = true,
+      -- insert_at_start = true,
+      -- …etc.
+    },
+    version = '^1.0.0', -- optional: only update when a new 1.x version is released
+  },
+  { 'akinsho/toggleterm.nvim', version = "*", config = true },
+})
+
+-- set options
+vim.opt.termguicolors = true
+vim.opt.number = true
+vim.opt.updatetime = 500
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+
+
+-- map prefix
+vim.g.mapleader = ' '
+vim.keymap.set({ 'n', 'x' }, '<Space>', '<Nop>')
+vim.keymap.set({ 'n', 'x' }, '<Plug>(lsp)', '<Nop>')
+vim.keymap.set({ 'n', 'x' }, 'm', '<Plug>(lsp)')
+vim.keymap.set({ 'n', 'x' }, '<Plug>(ff)', '<Nop>')
+vim.keymap.set({ 'n', 'x' }, ';', '<Plug>(ff)')
+vim.keymap.set('i', 'jk', '<Esc>')
+
+-- telescope.nvim
+require('telescope').setup({
+  defaults = {
+    mappings = {
+      n = {
+        ['<Esc>'] = require('telescope.actions').close,
+        ['<C-g>'] = require('telescope.actions').close,
+      },
+      i = {
+        ['<C-g>'] = require('telescope.actions').close,
+      },
+    },
+  },
+})
+vim.keymap.set({ 'n' }, '<Plug>(ff)r', '<Cmd>Telescope find_files<CR>')
+vim.keymap.set({ 'n' }, '<Plug>(ff)s', '<Cmd>Telescope git_status<CR>')
+vim.keymap.set({ 'n' }, '<Plug>(ff)b', '<Cmd>Telescope buffers<CR>')
+vim.keymap.set({ 'n' }, '<Plug>(ff)f', '<Cmd>Telescope live_grep<CR>')
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+-- nvim-lsp
+local lsp_config = require('lspconfig')
+local mason = require('mason')
+local mason_lspconfig = require('mason-lspconfig')
+local mason_null_ls = require('mason-null-ls')
+local null_ls = require('null-ls')
+
+require('dressing').setup()
+require('lspsaga').setup()
+require('lsp_signature').setup({ hint_enable = false })
+require('fidget').setup()
+
+mason.setup()
+mason_null_ls.setup({
+  ensure_installed = { 'prettier' },
+  automatic_installation = true,
+})
+null_ls.setup({
+  sources = { null_ls.builtins.formatting.prettier },
+})
+
+mason_lspconfig.setup({
+  ensure_installed = {
+    'tsserver',
+    'eslint',
+  },
+  automatic_installation = true,
+})
+
+mason_lspconfig.setup_handlers({
+  function(server_name)
+    local opts = {
+      capabilities = require('cmp_nvim_lsp').default_capabilities(),
+    }
+
+    lsp_config[server_name].setup(opts)
+  end,
+})
+
+vim.api.nvim_create_autocmd({ 'CursorHold' }, {
+  pattern = { '*' },
+  callback = function()
+    require('lspsaga.diagnostic').show_cursor_diagnostics()
+  end,
+})
+
+vim.api.nvim_create_autocmd({ 'FileType' }, {
+  pattern = { 'typescript', 'typescriptreact', 'typescript.tsx' },
+  callback = function()
+    vim.keymap.set({ 'n' }, '<Plug>(lsp)f', function()
+      vim.cmd([[EslintFixAll]])
+      vim.lsp.buf.format({ name = 'null-ls' })
+    end)
+  end,
+})
+
+local function show_documentation()
+  local ft = vim.opt.filetype._value
+  if ft == 'vim' or ft == 'help' then
+    vim.cmd([[execute 'h ' . expand('<cword>') ]])
+  else
+    require('lspsaga.hover').render_hover_doc()
+  end
+end
+
+vim.keymap.set({ 'n' }, 'K', show_documentation)
+vim.keymap.set({ 'n' }, '<Plug>(lsp)a', require('lspsaga.codeaction').code_action)
+vim.keymap.set({ 'n' }, '<Plug>(lsp)rn', require('lspsaga.rename').rename)
+vim.keymap.set({ 'n' }, '<Plug>(lsp)q', '<Cmd>Telescope diagnostics<CR>')
+vim.keymap.set({ 'n' }, '<Plug>(lsp)n', require('lspsaga.diagnostic').navigate('next'))
+vim.keymap.set({ 'n' }, '<Plug>(lsp)p', require('lspsaga.diagnostic').navigate('prev'))
+vim.keymap.set({ 'n' }, '<Plug>(lsp)f', vim.lsp.buf.format)
+vim.keymap.set({ 'n' }, '<Plug>(lsp)i', '<Cmd>Telescope lsp_implementations<CR>')
+vim.keymap.set({ 'n' }, '<Plug>(lsp)t', '<Cmd>Telescope lsp_type_definitions<CR>')
+vim.keymap.set({ 'n' }, '<Plug>(lsp)rf', '<Cmd>Telescope lsp_references<CR>')
+
+-- nvim-cmp
+local status, cmp = pcall(require, "cmp")
+if (not status) then return end
+local lspkind = require 'lspkind'
+
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.close(),
+    ['<CR>'] = cmp.mapping.confirm({
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true
+    }),
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'buffer' },
+  }),
+  formatting = {
+    format = lspkind.cmp_format({ with_text = false, maxwidth = 50 })
+  }
+})
+
+vim.cmd [[
+  set completeopt=menuone,noinsert,noselect
+  highlight! default link CmpItemKind CmpItemMenuDefault
+]]
+
+
+-- treesitter
+local status, ts = pcall(require, "nvim-treesitter.configs")
+if (not status) then return end
+
+ts.setup {
+  highlight = {
+    enable = true,
+    disable = {},
+  },
+  indent = {
+    enable = true,
+    disable = {},
+  },
+  ensure_installed = {
+    "tsx",
+    "toml",
+    "fish",
+    "php",
+    "json",
+    "yaml",
+    "css",
+    "html",
+    "lua"
+  },
+  autotag = {
+    enable = true,
+  },
+}
+
+-- fern.vim
+vim.keymap.set({ 'n' }, '<Leader>e', '<Cmd>Fern . -drawer<CR>')
+vim.keymap.set({ 'n' }, '<Leader>E', '<Cmd>Fern . -drawer -reveal=%<CR>')
+
+local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
+
+-- lualine
+local status, lualine = pcall(require, "lualine")
+if (not status) then return end
+
+lualine.setup {
+  options = {
+    icons_enabled = true,
+    theme = 'solarized_dark',
+    section_separators = { left = '', right = '' },
+    component_separators = { left = '', right = '' },
+    disabled_filetypes = {}
+  },
+  sections = {
+    lualine_a = { 'mode' },
+    lualine_b = { 'branch' },
+    lualine_c = { {
+      'filename',
+      file_status = true, -- displays file status (readonly status, modified status)
+      path = 0            -- 0 = just filename, 1 = relative path, 2 = absolute path
+    } },
+    lualine_x = {
+      { 'diagnostics', sources = { "nvim_diagnostic" }, symbols = { error = ' ', warn = ' ', info = ' ',
+        hint = ' ' } },
+      'encoding',
+      'filetype'
+    },
+    lualine_y = { 'progress' },
+    lualine_z = { 'location' }
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = { {
+      'filename',
+      file_status = true, -- displays file status (readonly status, modified status)
+      path = 1            -- 0 = just filename, 1 = relative path, 2 = absolute path
+    } },
+    lualine_x = { 'location' },
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  extensions = { 'fugitive' }
+}
+
+-- gruvbox
+vim.cmd.colorscheme('gruvbox-material')
+
+-- autopairs
+local status, autopairs = pcall(require, "nvim-autopairs")
+if (not status) then return end
+
+autopairs.setup({
+  disable_filetype = { "TelescopePrompt", "vim" },
+})
+
+-- format-on-save
+local format_on_save = require("format-on-save")
+local formatters = require("format-on-save.formatters")
+
+format_on_save.setup({
+  exclude_path_patterns = {
+    "/node_modules/",
+    ".local/share/nvim/lazy",
+  },
+  formatter_by_ft = {
+    css = formatters.lsp,
+    html = formatters.lsp,
+    java = formatters.lsp,
+    javascript = formatters.lsp,
+    json = formatters.lsp,
+    lua = formatters.lsp,
+    markdown = formatters.prettierd,
+    openscad = formatters.lsp,
+    python = formatters.black,
+    rust = formatters.lsp,
+    scad = formatters.lsp,
+    scss = formatters.lsp,
+    sh = formatters.shfmt,
+    terraform = formatters.lsp,
+    typescript = formatters.prettierd,
+    typescriptreact = formatters.prettierd,
+    yaml = formatters.lsp,
+
+    -- Add your own shell formatters:
+    myfiletype = formatters.shell({ cmd = { "myformatter", "%" } }),
+
+    -- Add lazy formatter that will only run when formatting:
+    my_custom_formatter = function()
+      if vim.api.nvim_buf_get_name(0):match("/README.md$") then
+        return formatters.prettierd
+      else
+        return formatters.lsp()
+      end
+    end,
+
+    -- Add custom formatter
+    filetype1 = formatters.remove_trailing_whitespace,
+    filetype2 = formatters.custom({
+      format = function(lines)
+        return vim.tbl_map(function(line)
+          return line:gsub("true", "false")
+        end, lines)
+      end
+    }),
+
+    -- Concatenate formatters
+    python = {
+      formatters.remove_trailing_whitespace,
+      formatters.shell({ cmd = "tidy-imports" }),
+      formatters.black,
+      formatters.ruff,
+    },
+
+    -- Use a tempfile instead of stdin
+    go = {
+      formatters.shell({
+        cmd = { "goimports-reviser", "-rm-unused", "-set-alias", "-format", "%" },
+        tempfile = function()
+          return vim.fn.expand("%") .. '.formatter-temp'
+        end
+      }),
+      formatters.shell({ cmd = { "gofmt" } }),
+    },
+
+    -- Add conditional formatter that only runs if a certain file exists
+    -- in one of the parent directories.
+    javascript = {
+      formatters.if_file_exists({
+        pattern = ".eslintrc.*",
+        formatter = formatters.eslint_d_fix
+      }),
+      formatters.if_file_exists({
+        pattern = { ".prettierrc", ".prettierrc.*", "prettier.config.*" },
+        formatter = formatters.prettierd,
+      }),
+      -- By default it stops at the git repo root (or "/" if git repo not found)
+      -- but it can be customized with the `stop_path` option:
+      formatters.if_file_exists({
+        pattern = ".prettierrc",
+        formatter = formatters.prettierd,
+        stop_path = function()
+          return "/my/custom/stop/path"
+        end
+      }),
+    },
+  },
+
+  -- Optional: fallback formatter to use when no formatters match the current filetype
+  fallback_formatter = {
+    formatters.remove_trailing_whitespace,
+    formatters.remove_trailing_newlines,
+    formatters.prettierd,
+  },
+
+  -- By default, all shell commands are prefixed with "sh -c" (see PR #3)
+  -- To prevent that set `run_with_sh` to `false`.
+  run_with_sh = false,
+})
+
+-- barbar
+local map = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+
+-- Move to previous/next
+map('n', '<A-,>', '<Cmd>BufferPrevious<CR>', opts)
+map('n', '<A-.>', '<Cmd>BufferNext<CR>', opts)
+-- Re-order to previous/next
+map('n', '<A-<>', '<Cmd>BufferMovePrevious<CR>', opts)
+map('n', '<A->>', '<Cmd>BufferMoveNext<CR>', opts)
+-- Goto buffer in position...
+map('n', '<A-1>', '<Cmd>BufferGoto 1<CR>', opts)
+map('n', '<A-2>', '<Cmd>BufferGoto 2<CR>', opts)
+map('n', '<A-3>', '<Cmd>BufferGoto 3<CR>', opts)
+map('n', '<A-4>', '<Cmd>BufferGoto 4<CR>', opts)
+map('n', '<A-5>', '<Cmd>BufferGoto 5<CR>', opts)
+map('n', '<A-6>', '<Cmd>BufferGoto 6<CR>', opts)
+map('n', '<A-7>', '<Cmd>BufferGoto 7<CR>', opts)
+map('n', '<A-8>', '<Cmd>BufferGoto 8<CR>', opts)
+map('n', '<A-9>', '<Cmd>BufferGoto 9<CR>', opts)
+map('n', '<A-0>', '<Cmd>BufferLast<CR>', opts)
+-- Pin/unpin buffer
+map('n', '<A-p>', '<Cmd>BufferPin<CR>', opts)
+-- Close buffer
+map('n', '<A-c>', '<Cmd>BufferClose<CR>', opts)
+-- Wipeout buffer
+--                 :BufferWipeout
+-- Close commands
+--                 :BufferCloseAllButCurrent
+--                 :BufferCloseAllButPinned
+--                 :BufferCloseAllButCurrentOrPinned
+--                 :BufferCloseBuffersLeft
+--                 :BufferCloseBuffersRight
+-- Magic buffer-picking mode
+map('n', '<C-p>', '<Cmd>BufferPick<CR>', opts)
+-- Sort automatically by...
+map('n', '<Space>bb', '<Cmd>BufferOrderByBufferNumber<CR>', opts)
+map('n', '<Space>bd', '<Cmd>BufferOrderByDirectory<CR>', opts)
+map('n', '<Space>bl', '<Cmd>BufferOrderByLanguage<CR>', opts)
+map('n', '<Space>bw', '<Cmd>BufferOrderByWindowNumber<CR>', opts)
+
+-- Other:
+-- :BarbarEnable - enables barbar (enabled by default)
+-- :BarbarDisable - very bad command, should never be used
+
+-- ToggleTerm
+require("toggleterm").setup {
+  direction = 'float',
+}
+vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+vim.keymap.set('n', '<leader>t', '<Cmd>ToggleTerm<CR>')
+vim.keymap.set('t', '<C-t>', [[<C-\><C-n>]])
---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,216 @@
+-- nvim-lsp
+local lsp_config = require('lspconfig')
+local mason = require('mason')
+local mason_lspconfig = require('mason-lspconfig')
+local mason_null_ls = require('mason-null-ls')
+local null_ls = require('null-ls')
+
+require('dressing').setup()
+require('lspsaga').setup()
+require('lsp_signature').setup({ hint_enable = false })
+require('fidget').setup()
+
+mason.setup()
+mason_null_ls.setup({
+  ensure_installed = { 'prettier' },
+  automatic_installation = true,
+})
+null_ls.setup({
+  sources = { null_ls.builtins.formatting.prettier },
+})
+
+mason_lspconfig.setup({
+  ensure_installed = {
+    'tsserver',
+    'eslint',
+  },
+  automatic_installation = true,
+})
+
+mason_lspconfig.setup_handlers({
+  function(server_name)
+    local opts = {
+      capabilities = require('cmp_nvim_lsp').default_capabilities(),
+    }
+
+    lsp_config[server_name].setup(opts)
+  end,
+})
+
+vim.api.nvim_create_autocmd({ 'CursorHold' }, {
+  pattern = { '*' },
+  callback = function()
+    require('lspsaga.diagnostic').show_cursor_diagnostics()
+  end,
+})
+
+vim.api.nvim_create_autocmd({ 'FileType' }, {
+  pattern = { 'typescript', 'typescriptreact', 'typescript.tsx' },
+  callback = function()
+    vim.keymap.set({ 'n' }, '<Plug>(lsp)f', function()
+      vim.cmd([[EslintFixAll]])
+      vim.lsp.buf.format({ name = 'null-ls' })
+    end)
+  end,
+})
+
+local function show_documentation()
+  local ft = vim.opt.filetype._value
+  if ft == 'vim' or ft == 'help' then
+    vim.cmd([[execute 'h ' . expand('<cword>') ]])
+  else
+    require('lspsaga.hover').render_hover_doc()
+  end
+end
+
+vim.keymap.set({ 'n' }, 'K', show_documentation)
+vim.keymap.set({ 'n' }, '<Plug>(lsp)a', require('lspsaga.codeaction').code_action)
+vim.keymap.set({ 'n' }, '<Plug>(lsp)rn', require('lspsaga.rename').rename)
+vim.keymap.set({ 'n' }, '<Plug>(lsp)q', '<Cmd>Telescope diagnostics<CR>')
+vim.keymap.set({ 'n' }, '<Plug>(lsp)n', require('lspsaga.diagnostic').navigate('next'))
+vim.keymap.set({ 'n' }, '<Plug>(lsp)p', require('lspsaga.diagnostic').navigate('prev'))
+vim.keymap.set({ 'n' }, '<Plug>(lsp)f', vim.lsp.buf.format)
+vim.keymap.set({ 'n' }, '<Plug>(lsp)i', '<Cmd>Telescope lsp_implementations<CR>')
+vim.keymap.set({ 'n' }, '<Plug>(lsp)t', '<Cmd>Telescope lsp_type_definitions<CR>')
+vim.keymap.set({ 'n' }, '<Plug>(lsp)rf', '<Cmd>Telescope lsp_references<CR>')
+
+return {
+  -- tools
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "stylua",
+        "selene",
+        "luacheck",
+        "shellcheck",
+        "shfmt",
+        "tailwindcss-language-server",
+        "typescript-language-server",
+        "css-lsp",
+      })
+    end,
+  },
+  'williamboman/mason-lspconfig',
+  'jayp0521/mason-null-ls.nvim',
+  -- lsp servers
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      inlay_hints = { enabled = true },
+      ---@type lspconfig.options
+      servers = {
+        cssls = {},
+        tailwindcss = {
+          root_dir = function(...)
+            return require("lspconfig.util").root_pattern(".git")(...)
+          end,
+        },
+        tsserver = {
+          root_dir = function(...)
+            return require("lspconfig.util").root_pattern(".git")(...)
+          end,
+          single_file_support = false,
+          settings = {
+            typescript = {
+              inlayHints = {
+                includeInlayParameterNameHints = "literal",
+                includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+                includeInlayFunctionParameterTypeHints = true,
+                includeInlayVariableTypeHints = false,
+                includeInlayPropertyDeclarationTypeHints = true,
+                includeInlayFunctionLikeReturnTypeHints = true,
+                includeInlayEnumMemberValueHints = true,
+              },
+            },
+            javascript = {
+              inlayHints = {
+                includeInlayParameterNameHints = "all",
+                includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+                includeInlayFunctionParameterTypeHints = true,
+                includeInlayVariableTypeHints = true,
+                includeInlayPropertyDeclarationTypeHints = true,
+                includeInlayFunctionLikeReturnTypeHints = true,
+                includeInlayEnumMemberValueHints = true,
+              },
+            },
+          },
+        },
+        html = {},
+        yamlls = {
+          settings = {
+            yaml = {
+              keyOrdering = false,
+            },
+          },
+        },
+        lua_ls = {
+          -- enabled = false,
+          single_file_support = true,
+          settings = {
+            Lua = {
+              workspace = {
+                checkThirdParty = false,
+              },
+              completion = {
+                workspaceWord = true,
+                callSnippet = "Both",
+              },
+              misc = {
+                parameters = {
+                  -- "--log-level=trace",
+                },
+              },
+              hint = {
+                enable = true,
+                setType = false,
+                paramType = true,
+                paramName = "Disable",
+                semicolon = "Disable",
+                arrayIndex = "Disable",
+              },
+              doc = {
+                privateName = { "^_" },
+              },
+              type = {
+                castNumberToInteger = true,
+              },
+              diagnostics = {
+                disable = { "incomplete-signature-doc", "trailing-space" },
+                -- enable = false,
+                groupSeverity = {
+                  strong = "Warning",
+                  strict = "Warning",
+                },
+                groupFileStatus = {
+                  ["ambiguity"] = "Opened",
+                  ["await"] = "Opened",
+                  ["codestyle"] = "None",
+                  ["duplicate"] = "Opened",
+                  ["global"] = "Opened",
+                  ["luadoc"] = "Opened",
+                  ["redefined"] = "Opened",
+                  ["strict"] = "Opened",
+                  ["strong"] = "Opened",
+                  ["type-check"] = "Opened",
+                  ["unbalanced"] = "Opened",
+                  ["unused"] = "Opened",
+                },
+                unusedLocalExclude = { "_*" },
+              },
+              format = {
+                enable = false,
+                defaultConfig = {
+                  indent_style = "space",
+                  indent_size = "2",
+                  continuation_indent_size = "2",
+                },
+              },
+            },
+          },
+        },
+      },
+      setup = {},
+    },
+  },
+}
